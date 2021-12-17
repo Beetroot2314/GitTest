5479af6296a52c08f3bc1530d303501ff9b75ceb
Beetroot2314 <amberkumar123827@gmail.com>
2021-12-17 15:44:57 +0530

    Commit 1

---
+++ src/gitttest/Palindrome.java
@@ -0,0 +1,32 @@
+package gitttest;
+import java.io.*;
+
+
+public class Palindrome {
+	
+	public static void main(String args[])throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter a number");
+		int a=Integer.parseInt(br.readLine());
+		int temp=a;
+		int rem;
+		int rev=0;
+		
+		while(temp>0)
+		{
+		rem=temp%10;
+		rev=(rev*10)+rem;
+		temp=temp/10;
+		}
+		
+		if(a==rev)
+			System.out.println("Palindrome");
+		else
+			System.out.println("Not palindrome");
+		
+		
+		
+	}
+
+}
